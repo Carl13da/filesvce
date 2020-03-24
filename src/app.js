88a57e3ec865655391f529c9fe66cd21e7adfa3d
Carlos Santos <carloshenrique0322@gmail.com>
2020-03-24 03:46:08 -0300

    changing header on middleware

--- src/app.js
+++ src/app.js
@@ -9,15 +9,16 @@ class App {
     this.server = express();
 
     this.server.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header(
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
       );
-      res.header(
+      res.setHeader(
         'Access-Control-Allow-Methods',
         'PUT, POST, GET, DELETE, OPTIONS'
       );
+      res.setHeader('Access-Control-Allow-Credentials', true);
       next();
     });
 
